b57d592dc9095d60e0a8a77afa5573a0bd9745c2
Keenan Payne <keenanpayne504@gmail.com>
2022-08-25 15:18:55 -0500

    phase 1 pt1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
 
-// backend/routes/index.js
-// ...
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
